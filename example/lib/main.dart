4b7b22a064f5deaab8f34e2d725ddd128c18636a
user <git@sgougeon.fr>
2021-09-18 12:52:07 +0200

    Add custom distributor picker to the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:unifiedpush/unifiedpush.dart';
 import 'package:http/http.dart' as http;
@@ -91,6 +93,15 @@ class HomePage extends StatelessWidget {
       body: "title=${title.text}&message=${message.text}&priority=6"
   );
 
+  String myPickerFunc(List<String> distributors) {
+    // Do not do a random func, this is an example.
+    // You should do a context menu/dialog here
+    Random rand = new Random();
+    final max = distributors.length;
+    final index = rand.nextInt(max);
+    return distributors[index];
+  }
+
   @override
   Widget build(BuildContext context) {
     List<Widget> row = [
@@ -100,7 +111,24 @@ class HomePage extends StatelessWidget {
           if (registered) {
             UnifiedPush.unregister();
           } else {
+            /**
+             * Option 1:  Use the default distributor picker
+             *            which uses a dialog
+             */
             UnifiedPush.registerAppWithDialog();
+            /**
+             * Option 2: Do your own function to pick the distrib
+             */
+            /*
+            if (await UnifiedPush.getDistributor() != "") {
+              UnifiedPush.registerApp();
+            } else {
+              final distributors = await UnifiedPush.getDistributors();
+              final distributor = myPickerFunc(distributors);
+              UnifiedPush.saveDistributor(distributor);
+              UnifiedPush.registerApp();
+            }
+            */
           }
         },
       ),

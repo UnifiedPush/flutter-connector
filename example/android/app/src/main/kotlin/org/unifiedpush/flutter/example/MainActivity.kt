c5a45b456ec9aebe209c97b58187721e78295576
sim <git@sgougeon.fr>
2022-03-07 09:14:06 +0100

    Use post 1.12 flutter android acivity

--- example/android/app/src/main/kotlin/org/unifiedpush/flutter/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/org/unifiedpush/flutter/example/MainActivity.kt
@@ -1,15 +1,10 @@
 package org.unifiedpush.flutter.example
 
 import android.content.Context
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    };
-
     override fun provideFlutterEngine(context: Context): FlutterEngine? {
         return provideEngine(this)
     }
@@ -21,7 +16,7 @@ class MainActivity : FlutterActivity() {
     companion object {
         var engine: FlutterEngine? = null
         fun provideEngine(context: Context): FlutterEngine {
-            var eng = engine ?: FlutterEngine(context, emptyArray(), true, false)
+            val eng = engine ?: FlutterEngine(context, emptyArray(), true, false)
             engine = eng
             return eng
         }

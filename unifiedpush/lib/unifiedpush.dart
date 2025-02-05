c8265e7e199ce1af06dcec70a29ae443e45437f6
sim <git@sgougeon.fr>
2025-02-05 11:35:05 +0000

    Remove deprecated dialog

--- unifiedpush/lib/unifiedpush.dart
+++ unifiedpush/lib/unifiedpush.dart
@@ -1,11 +1,8 @@
 import 'dart:async';
 import 'dart:typed_data';
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:unifiedpush_platform_interface/unifiedpush_platform_interface.dart';
 
 import 'constants.dart';
-import 'dialogs.dart';
 
 class UnifiedPush {
   static Future<void> initialize({
@@ -21,46 +18,6 @@ class UnifiedPush {
         onMessage: (Uint8List m, String i) async => onMessage?.call(m, i));
   }
 
-  static const noDistribAck = "noDistributorAck";
-
-  @Deprecated("Use UnifiedPushUI")
-  static Future<void> registerAppWithDialog(BuildContext context,
-      [String instance = defaultInstance, List<String>? features]) async {
-    var distributor = await getDistributor();
-    final prefs = await SharedPreferences.getInstance();
-    String? picked;
-
-    if (distributor == null) {
-      final distributors = await getDistributors(features = features);
-      if (distributors.isEmpty) {
-        if (!(prefs.getBool(noDistribAck) ?? false)) {
-          return showDialog(
-              context: context,
-              builder: noDistributorDialog(onDismissed: () {
-                prefs.setBool(noDistribAck, true);
-              }));
-        }
-      } else if (distributors.length == 1) {
-        picked = distributors.single;
-      } else {
-        picked = await showDialog<String>(
-          context: context,
-          builder: pickDistributorDialog(distributors),
-        );
-      }
-
-      if (picked != null) {
-        await saveDistributor(picked);
-      }
-    }
-
-    await registerApp(instance = instance, features = features);
-  }
-
-  static Future<void> removeNoDistributorDialogACK() async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.remove(noDistribAck);
-  }
 
   static Future<void> registerApp(
       [String instance = defaultInstance,

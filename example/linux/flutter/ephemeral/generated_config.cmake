e6f00c26d21d5a6b9b1814693a53af7b98b58844
Bart Ribbers <bribbers@disroot.org>
2025-02-18 08:11:02 +0100

    chore(gitignore): ignore FVM related directories
    And when using fvm use Flutter 3.10.0. It's the first minor
    release shipping with Dart 3.0.

---
+++ example/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/bart/fvm/versions/3.27.4" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/bart/Documents/Git/fpas/flutter-connector/example" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/bart/fvm/versions/3.27.4"
+  "PROJECT_DIR=/home/bart/Documents/Git/fpas/flutter-connector/example"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/bart/Documents/Git/fpas/flutter-connector/example/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/bart/Documents/Git/fpas/flutter-connector/example/lib/main.dart"
+)
